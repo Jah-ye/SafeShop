2a2d424d292ba609d9d9fb8004178ffc5fb80428
Jah-ye <jaawa001@osloskolen.no>
2026-06-12 12:49:07 +0200

    endring

--- frontend/script.js
+++ frontend/script.js
@@ -10,7 +10,7 @@ async function sendBestilling(event) {
         melding.textContent = "Sender bestilling...";
         
         try {
-            let response = await fetch('', {
+            let response = await fetch('http://192.168.20.62:5000/', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ 
